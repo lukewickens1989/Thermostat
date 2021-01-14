4a0dc0035a69b41fffb8c709c57d832d0d5043c5
Luke Wickens <lukewickens1989@gmail.com>
2021-01-14 14:42:58 +0000

    Second specification completed - added up function that takes increase amount as param.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,5 +11,28 @@ describe("Thermostat", function(){
       expect(thermostat.default_temp).toEqual(20)
     })
   });
+
+  describe('#up', () => {
+
+    beforeEach( () => {
+      thermostat.current_temp = thermostat.default_temp
+    });
+
+    it('should turn up the temperature by passed in amount', () => {
+      thermostat.up(1)
+      expect(thermostat.current_temp).toEqual(21)
+    });
+
+    it('should turn up the temperature by 2', () => {
+      thermostat.up(2)
+      expect(thermostat.current_temp).toEqual(22)
+    });
+
+    it('should turn up the temperature by 5', () => {
+      thermostat.up(5)
+      expect(thermostat.current_temp).toEqual(25)
+    });
+
+  });
   
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,5 +3,10 @@ class Thermostat {
   
   constructor() {
     this.default_temp = 20;
+    this.current_temp = this.default_temp
+  }
+
+  up(amount) {
+    this.current_temp += amount
   }
 }
